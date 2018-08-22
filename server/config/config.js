33dfe3297b0b2696f81c7aebc8ecfa5d1c53f5bb
Gabriel Walsh <gabriel.walsh@moleskine.com>
2018-08-22 07:06:55 -0400

    modification to sequelize config

--- server/config/config.js
+++ server/config/config.js
@@ -15,13 +15,5 @@ module.exports = {
     host: process.env.TEST_DB_HOSTNAME,
     port: process.env.TEST_DB_PORT,
     dialect: 'postgres'
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
   }
 };
